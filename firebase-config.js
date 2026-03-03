e09c61f85ad6f2ebfe76d00a3daf8a8ab493421a
ok7bi <alihaidaralajmi1980@gmail.com>
2026-03-03 14:42:30 +0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD6p7C_bwOgqxgSIURBHD7weOgGFQG01F4",
-  authDomain: "layali-ramadan.firebaseapp.com",
-  databaseURL: "https://layali-ramadan-default-rtdb.firebaseio.com",
-  projectId: "layali-ramadan",
-  storageBucket: "layali-ramadan.firebasestorage.app",
-  messagingSenderId: "71542361162",
-  appId: "1:71542361162:web:3a2e63ce5af08c9f702ab3"
-};
